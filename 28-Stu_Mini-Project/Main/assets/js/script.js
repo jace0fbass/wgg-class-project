d735321e7ca7af134f40c525811d2c5eb49e54d2
Jason <jajones414@gmail.com>
2022-09-19 23:03:26 -0500

    I have followed along with the class to create the word guess game

--- 28-Stu_Mini-Project/Main/assets/js/script.js
+++ 28-Stu_Mini-Project/Main/assets/js/script.js
@@ -1,25 +1,69 @@
 var startButton = document.querySelector(".start-button")
-var wordBlanksEl = document.querySelector(".wordblanks")
+var wordBlanksEl = document.querySelector(".word-blanks")
+var scoreEl = document.querySelector(".score")
+var timerEl = document.querySelector(".timer-count")
 
-var word
+var validChars = "abcdefghijklmnopqrstuvwxyz"
 var words = ["golf-ball", "par", "slice", "driver", "putter", "cart"]
+var word
+var guessedChars = []
+var score = 0
+var timeLeft = 15
+var intervalId
 // score var
 // time left var
+function startCountdown() {
+  clearInterval(intervalId)
+  intervalId = setInterval(function() {
+    timeLeft--
+    timerEl.textContent = timeLeft
+    if (timeLeft === 0) {
+      clearInterval(intervalId)
+    wordBlanksEl.innerText = "Game over! Your score is " + score
+  }
+  }, 1000)
+}
+
+function checkWord() {
+  var wordFromDOM = wordBlanksEl.textContent.split(" ").join(" ")
+  if (word === wordFromDOM) {
+    score++
+    scoreEl.textContent = score
+    startRound()
+  }
+}
+
+function handleKeydown(event) {
+if (validChars.includes(event.key)) {
+guessedChars.push(event.key)
+renderCharacters()
+  }
+}
 
 function renderChars() {
-console.log(word)
-for(var i = 0; i < word.length; i++) {
-
+  var string = ""
+for (var i = 0; i < word.length; i++) {
+  var letter = word[i]
+  if (guessedChars.includes(letter) ) {
+    string += letter + " "
+  } else { 
+  string += "_ "
+  }
 }
+wordBlanksEl.textContent = string.trim()
+checkWord()
 }
 
 function startRound() {
+  guessedChars = []
   var randomIndex = Math.floor(Math.random() * words.length)
   word = words[randomIndex]
   renderChars()
+  startCountdown()
 }
 
 
 startButton.addEventListener("click", startRound)
 
+document.addEventListener("keydown", handleKeydown)
 // addEventListener
